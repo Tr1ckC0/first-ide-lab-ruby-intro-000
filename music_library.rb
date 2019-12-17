44f40ad7f63462d8c70f38016f34d394fe7ffbc8
Patrick Colina <trick.co@yahoo.com>
2019-12-17 21:44:59 +0000

    Automatically backed up by Learn

--- music_library.rb
+++ music_library.rb
@@ -3,12 +3,12 @@ music_library = [
     ["JESUS IS KING",
       ["Every Hour", "Selah", "Follow God"]
     ]
-  ]
+  ],
   ["Kendrick Lamar",
     ["DAMN.",
       ["YAH.", "DNA", "Fear."]
     ]
-  ]
+  ],
   ["Ms. Lauryn Hill",
     ["The Misedcuation Of Lauryn Hill",
       ["Do-op", "To Zion", "Ex-factor"]
