82ebec19e32f706469fc8b316d15191747cf6a72
Patrick Colina <trick.co@yahoo.com>
2019-12-17 21:42:00 +0000

    Automatically backed up by Learn

--- music_library.rb
+++ music_library.rb
@@ -15,3 +15,24 @@ music_library = [
     ]
   ]
 ]
+
+music_library.each do |artist_array|
+  artist_array.each do |artist_element|
+    if artist_element.class != Array
+      puts "Artist: #{artist_element}"
+    else
+      artist_element.each do |album_element|
+
+        if album_element.class != Array
+          puts "Album: #{album_element}"
+        else
+          album_element.each do |song_element|
+
+            if song_element.class != Array
+              puts "Song: #{song_element}"
+            end
+          end
+        end
+      end
+    end
+  end
