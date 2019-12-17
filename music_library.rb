84cf19b26908d5fd907949087026e59e9c41edff
Patrick Colina <trick.co@yahoo.com>
2019-12-17 21:33:32 +0000

    Automatically backed up by Learn

---
+++ music_library.rb
@@ -0,0 +1,17 @@
+music_library = [
+  ["Kanye West",
+    ["JESUS IS KING",
+      ["Every Hour", "Selah", "Follow God"]
+    ]
+  ]
+  ["Kendrick Lamar"
+    ["DAMN.",
+      ["YAH.", "DNA", "Fear."]
+    ]
+  ]
+  ["Ms. Lauryn Hill",
+    ["The Misedcuation Of Lauryn Hill",
+      ["Do-op", "To Zion", "Ex-factor"]
+    ]
+  ]
+]
